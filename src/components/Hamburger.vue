ddbbd5ab09f4f577842bbcbaa49a28a7c17c1efe
pei-pay <gleektennis@icloud.com>
2022-04-30 12:43:59 +0900

    feat: header実装

---
+++ src/components/Hamburger.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+defineProps({
+  isOpened: Boolean
+})
+</script>
+
+<template>
+  <div id="hamburger" :class="{ open: isOpened }" @click="$emit('click:hamburger-menu')">
+    <div class="hamburger__icon">
+      <span class="hamburger__icon--top"></span>
+      <span class="hamburger__icon--middle"></span>
+      <span class="hamburger__icon--bottom"></span>
+    </div>
+  </div>
+</template>
+
+<style lang="sass">
+@import '../assets/style/_mixins.sass'
+@import '../assets/style/_variables.sass'
+
+#hamburger
+  .hamburger__icon
+    position: relative
+    height: 1.5rem
+    width: 1.8rem
+    .hamburger__icon--top,
+    .hamburger__icon--middle,
+    .hamburger__icon--bottom
+      position: absolute
+      display: block
+      height: 0.25rem
+      width: 1.8rem
+      border-radius: 2px
+      background-color: $primary
+      transition: all 0.3s cubic-bezier(0.27, -0.72, 0.79, 1.84)
+
+    .hamburger__icon--top
+      top: 0
+    .hamburger__icon--middle
+      top: 0.625rem
+    .hamburger__icon--bottom
+      bottom: 0
+
+  &.open
+    .hamburger__icon--top
+      transform: translateY(10px) rotate(-45deg)
+
+    .hamburger__icon--middle
+      opacity: 0
+
+    .hamburger__icon--bottom
+      transform: translateY(-10px) rotate(45deg)
+
+  +breakpoint-up(large)
+    display: none
+    z-index: 2
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,77 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import Hamburger from './Hamburger.vue'
+
+const isOpenedHamburgerMenu = ref(false)
+const toggleHamburgerMenu = (): void => {
+  isOpenedHamburgerMenu.value = !isOpenedHamburgerMenu.value
+}
+</script>
+
+<template>
+  <header class="nav-bar">
+    <nav>
+      <!-- TODO: 達成したいことが入る -->
+      <h1 class="title">Study English</h1>
+      <Hamburger
+        :is-opened="isOpenedHamburgerMenu"
+        @click:hamburger-menu="toggleHamburgerMenu"
+      />
+      <ul class="menu-items" :class="{ open: isOpenedHamburgerMenu }">
+        <li><a>calendar</a></li>
+        <li><a>chart</a></li>
+        <li><a>setting</a></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style lang="sass">
+@import '../assets/style/_mixins.sass'
+@import '../assets/style/_variables.sass'
+
+header.nav-bar
+  position: fixed
+  width: 100%
+  nav
+    height: 3rem
+    display: flex
+    justify-content: space-between
+    color: $primary
+    padding: 1.5rem
+
+    .menu-items
+      display: flex
+      li
+        font-size: 1.25rem
+        cursor: pointer
+        transition: all 0.3s ease-in-out
+        &:hover
+          opacity: 0.7
+          border-bottom: 5px solid $secondary
+          transition: all 0.3s ease-in-out
+        +breakpoint-up(large)
+          margin-left: 1.5rem
+
+      &.open
+        visibility: visible
+        opacity: 1
+        transform: translateX(0)
+
+      +breakpoint-down(medium)
+        position: absolute
+        right: 0
+        flex-direction: column
+        margin-top: 4rem
+        height: 100vh
+        width: 30vw
+        max-width: 10rem
+        visibility: hidden
+        opacity: 0
+        transform: translateX(100%)
+        transition: all 0.3s ease-in-out
+        li
+          margin-bottom: 1rem
+          margin-left: .5rem
+          cursor: pointer
+</style>
