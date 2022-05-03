20a7b542ef1c20e8a4942c14cc13eacf637f8ced
pei-pay <gleektennis@icloud.com>
2022-05-03 14:28:08 +0900

    add fullCalendar

--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -1,3 +1,14 @@
+<script setup>
+import '@fullcalendar/core/vdom' // solves problem with Vite
+import FullCalendar from '@fullcalendar/vue3'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
+const calendarOptions = {
+  plugins: [dayGridPlugin, interactionPlugin],
+  initialView: 'dayGridMonth'
+}
+</script>
+
 <template>
-  <div>calendar</div>
+  <FullCalendar :options="calendarOptions" />
 </template>
